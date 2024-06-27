2e75f1d43807bd022afb6919700f1a2e4a0ca3ae
Juan Francisco Herrera Morejon <jfhm56@gmail.com>
2024-06-27 16:37:03 -0300

    se configura bolirplate de frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListVehiclesComponent } from './components/list-vehicles/list-vehicles.component';
+import { AddEditVehicleComponent } from './components/add-edit-vehicle/add-edit-vehicle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ListVehiclesComponent,
+    AddEditVehicleComponent
   ],
   imports: [
     BrowserModule,

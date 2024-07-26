f3a19da725618cb5c155ab34d49f642c53a4997f
BrianOrtigosa <brianchu44@gmail.com>
2024-07-26 09:39:59 -0300

    Añadir archivos no rastreados antes de hacer pull

--- backend/dist/vehicleTypes/vehicleType.entity.js
+++
@@ -1,9 +0,0 @@
-import crypto from 'node:crypto';
-export class VehicleType {
-    constructor(nombre, codigo, id = crypto.randomUUID()) {
-        this.nombre = nombre;
-        this.codigo = codigo;
-        this.id = id;
-    }
-}
-//# sourceMappingURL=vehicleType.entity.js.map
--- backend/dist/vehicles/vehicles.entity.js
+++
@@ -1,9 +0,0 @@
-import crypto from 'node:crypto';
-export class Vehicle {
-    constructor(marca, patente, id = crypto.randomUUID()) {
-        this.marca = marca;
-        this.patente = patente;
-        this.id = id;
-    }
-}
-//# sourceMappingURL=vehicles.entity.js.map

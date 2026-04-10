aa8b9fd93092c6afd650d88196de077e375b894d
BrianOrtigosa <brianchu44@gmail.com>
2026-04-10 11:35:42 -0300

    arreglo environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'empathetic-forgiveness-production.up.railway.app'
+  apiUrl: 'https://empathetic-forgiveness-production.up.railway.app'
 };
